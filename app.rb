51960a47231a769d8a80e71a34b474d796807ead
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-18 08:33:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/name' do 
+  get '/name' do
+
+  end
+
+  get '/hometown' do
+
+  end
+
+  get '/favourite-song' do
 
   end
 end
