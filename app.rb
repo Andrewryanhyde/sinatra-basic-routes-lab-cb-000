fb18bc2374c731daaf501956b311bdd61f84a275
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-18 08:34:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,14 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-
+    "My name is Andrew"
   end
 
   get '/hometown' do
-
+    "My hometown is London"
   end
 
   get '/favourite-song' do
-
+    "My favourite song is Primavera"
   end
 end
